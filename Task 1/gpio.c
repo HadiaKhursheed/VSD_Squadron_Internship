d30793d527fc7c86f863d5c7de7e03196ed7f1aa
HadiaKhursheed <casperskyscraper@gmail.com>
2026-01-15 17:23:39 +0530

    Update gpio.c

--- Task 1/gpio.c
+++ Task 1/gpio.c
@@ -1,1 +1,22 @@
-
+/* This file shows how a library API is implemented */
+/* The Library API code is the one in gpio.h file */
+/* This file simulates a GPIO firmware library */
+#include <stdio.h> 
+#include "gpio.h"
+void gpio_init(int pin, int direction)
+{
+    if (direction == GPIO_OUTPUT) {
+        printf("GPIO %d initialized as OUTPUT\n", pin);
+    } else {
+        printf("GPIO %d initialized as INPUT\n", pin);
+    }
+}
+void gpio_write(int pin, int value)
+{
+    printf("GPIO %d write value: %d\n", pin, value);
+}
+int gpio_read(int pin)
+{
+    printf("GPIO %d read value\n", pin);
+    return 1; // simulated value
+}
