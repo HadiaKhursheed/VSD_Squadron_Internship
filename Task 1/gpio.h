165ccbd2fe41e7fe7cbfb588c1f60b0151b04e00
HadiaKhursheed <casperskyscraper@gmail.com>
2026-01-15 17:30:00 +0530

    Update gpio.h

--- Task 1/gpio.h
+++ Task 1/gpio.h
@@ -1,1 +1,13 @@
-
+/* GPIO Library API code */ 
+/* This GPIO Library code can be reused in various applications */ 
+/* Simple GPIO library (simulation) */
+#ifndef GPIO_H
+#define GPIO_H
+/* GPIO direction definitions */
+#define GPIO_OUTPUT 1
+#define GPIO_INPUT  0
+/* API functions */
+void gpio_init(int pin, int direction);
+void gpio_write(int pin, int value);
+int  gpio_read(int pin);
+#endif
